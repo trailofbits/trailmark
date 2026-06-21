67eb799e3430a70cdbe876725e0b64084439791a
dz <dzmitry@lahoda.pro>
2026-06-21 18:17:00 -0300

    refactor flake custom grammar packages

--- flake.nix
+++ flake.nix
@@ -21,6 +21,49 @@
           pythonPackages = python.pkgs;
           pyproject = builtins.fromTOML (builtins.readFile ./pyproject.toml);
 
+          treeSitterCustomGrammar =
+            grammar:
+            pkgs.stdenv.mkDerivation {
+              pname = "trailmark-tree-sitter-${grammar}";
+              version = pyproject.project.version;
+
+              src = ./src/trailmark/tree_sitter_custom/${grammar};
+
+              dontConfigure = true;
+
+              nativeBuildInputs = [
+                pkgs.stdenv.cc
+              ];
+
+              buildPhase = ''
+                runHook preBuild
+
+                ext_suffix="$(${python.interpreter} -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX") or ".so")')"
+                darwin_flags=(${pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isDarwin "-undefined dynamic_lookup"})
+
+                cc -shared -fPIC -O2 -std=c11 "''${darwin_flags[@]}" \
+                  -I"${python}/include/${python.libPrefix}" \
+                  -I"$src/src" \
+                  "$src/binding.c" \
+                  "$src/src/parser.c" \
+                  -o "_binding$ext_suffix"
+
+                runHook postBuild
+              '';
+
+              installPhase = ''
+                runHook preInstall
+
+                mkdir -p "$out"
+                cp _binding.* "$out/"
+
+                runHook postInstall
+              '';
+            };
+
+          tree-sitter-circom = treeSitterCustomGrammar "circom";
+          tree-sitter-masm = treeSitterCustomGrammar "masm";
+
           trailmark = pythonPackages.buildPythonApplication {
             pname = "trailmark";
             version = pyproject.project.version;
@@ -40,7 +83,6 @@
 
             nativeBuildInputs = [
               pythonPackages.pythonRelaxDepsHook
-              pkgs.stdenv.cc
             ];
 
             pythonRelaxDeps = [
@@ -48,25 +90,8 @@
             ];
 
             postInstall = ''
-              ext_suffix="$(${python.interpreter} -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX") or ".so")')"
-
-              build_tree_sitter_grammar() {
-                local grammar="$1"
-                local grammar_dir="$out/${python.sitePackages}/trailmark/tree_sitter_custom/$grammar"
-                local src_dir="$grammar_dir/src"
-                local output="$grammar_dir/_binding$ext_suffix"
-                local darwin_flags=(${pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isDarwin "-undefined dynamic_lookup"})
-
-                cc -shared -fPIC -O2 -std=c11 "''${darwin_flags[@]}" \
-                  -I"${python}/include/${python.libPrefix}" \
-                  -I"$src_dir" \
-                  "$grammar_dir/binding.c" \
-                  "$src_dir/parser.c" \
-                  -o "$output"
-              }
-
-              build_tree_sitter_grammar circom
-              build_tree_sitter_grammar masm
+              cp ${tree-sitter-circom}/_binding.* "$out/${python.sitePackages}/trailmark/tree_sitter_custom/circom/"
+              cp ${tree-sitter-masm}/_binding.* "$out/${python.sitePackages}/trailmark/tree_sitter_custom/masm/"
             '';
 
             pythonImportsCheck = [
@@ -83,7 +108,7 @@
           };
         in
         {
-          inherit trailmark;
+          inherit trailmark tree-sitter-circom tree-sitter-masm;
           default = trailmark;
         }
       );
